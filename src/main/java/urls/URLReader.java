1b72774b5e3ea8066b051df76d5b40918bf0abd3
Christian Alfonso Romero Martinez <christian.romero-m@mail.escuelaing.edu.co>
2026-02-17 19:44:01 -0500

    Ejercicio 1

--- src/main/java/urls/URLReader.java
+++ src/main/java/urls/URLReader.java
@@ -8,6 +8,18 @@ public class URLReader {
 
     public static void main(String[] args) throws Exception { 
         URL google = new URL("http://www.google.com/"); 
+        
+        // Imprimir información de la URL
+        System.out.println("Protocol: " + google.getProtocol());
+        System.out.println("Authority: " + google.getAuthority());
+        System.out.println("Host: " + google.getHost());
+        System.out.println("Port: " + google.getPort());
+        System.out.println("Path: " + google.getPath());
+        System.out.println("Query: " + google.getQuery());
+        System.out.println("File: " + google.getFile());
+        System.out.println("Ref: " + google.getRef());
+        System.out.println("contenido: ");
+        
         try (BufferedReader reader 
                 = new BufferedReader(new InputStreamReader(google.openStream()))) { 
             String inputLine = null; 
